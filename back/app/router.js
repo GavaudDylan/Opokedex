6a34f3d469c450bacfe7b1a63c2a570b20c1db97
Dylan GAVAUD <dylan.gavaud.dev@gmail.com>
2024-07-08 12:09:12 +0200

    Ajout des routes

---
+++ back/app/router.js
@@ -0,0 +1,33 @@
+import { Router } from "express";
+export const router = Router();
+
+// Pokémons
+router.get("/pokemons", getAllpokemons);
+router.get("/pokemons/:id", getPokemonById);
+
+// Types
+router.get("/types", getAllTypes);
+router.get("/types/:id", getTypeBId);
+
+// Équipes
+router.get("/teams", GetAllTeams);
+router.get("/teams/:id", getTeamById);
+router.post("/teams", createTeam);
+router.patch("/teams/:id", patchTeam);
+router.delete("/teams/:id", deleteTeam);
+// Notes :
+// - on ne doit pas pouvoir mettre deux fois le même Pokémon dans une même Team ;
+// - on ne doit pas pouvoir mettre plus de 6 Pokémons dans une Team.
+
+router.put("/teams/:id/pokemons/:id", patchPokemonsInTeam);
+router.delete("/teams/:id/pokemons/:id", deletePokemonsInTeam);
+
+// Votes
+router.post("/pokemons/:id/votes", currentVotes);
+router.get("/pokemons/leadboard", topTenPokemonsLeaderboard);
+// Notes :
+// - la route POST permet d'ajouter une voix supplémentaire à un Pokémon ;
+// - dans un premier temps, les utilisateurs peuvent l'appeler autant de fois qu'ils le souhaitent ;
+// - dans un second temps, il faudrait :
+//   - limiter cette route à un appel par utilisateur ;
+//   - offrir la possibilité à un utilisateur de retirer son vote via une route additionnelle.
